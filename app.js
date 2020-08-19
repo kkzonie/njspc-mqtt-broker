fcce4475586c9af459751202d9d5a806afaa90f8
kkzonie <kkzonie11@gmail.com>
2020-08-19 13:33:45 -0700

    minor update

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //glogal variables
 var mqttConnected = false;
 
+
 //config
 if (process.env.NODE_ENV == undefined) { process.env.NODE_ENV = "config" }
 
